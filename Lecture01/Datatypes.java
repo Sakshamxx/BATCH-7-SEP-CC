bdb601257de7bef87ffc6dd8ac2ba478ef7ea7fc
RYUGA <RYUGA@DESKTOP-IUGH0QO>
2024-09-22 13:13:51 +0530

    LECTURE

---
+++ Lecture01/Datatypes.java
@@ -0,0 +1,10 @@
+package Lecture01;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        // Primitive Datatypes
+        int a = 20;
+        System.out.print(a);
+        System.out.println(a);
+    }
+}
